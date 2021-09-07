b3665ddc955ecd68be326bbb5bd1aa7eb48931e0
Hong-Su Park <heavyflood@gmail.com>
2021-09-07 11:19:24 +0900

    133

--- src/main/java/com/phs/scg/DefaultController.java
+++ src/main/java/com/phs/scg/DefaultController.java
@@ -1,5 +1,7 @@
 package com.phs.scg;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,12 @@ import reactor.core.publisher.Mono;
 @RestController
 public class DefaultController {
 	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/")
     public Mono<String> getUser(ServerHttpRequest request, ServerHttpResponse response) {
 
+    	logger.info("This is Gateway MicroService!!!!!");
         return Mono.just("This is Gateway MicroService!!!!!");
     }
 
